2ea07c924f451bf5b1348a03c1cb518cb545f0cf
David Romaní <david@flux.cat>
2020-04-29 13:42:09 +0200

    improve FoldingCommand output style

--- src/Command/FoldingCommand.php
+++ src/Command/FoldingCommand.php
@@ -4,24 +4,29 @@ namespace App\Command;
 
 use App\Manager\FoldingTeamsApiManager;
 use App\Model\FoldingTeamMemberAccount;
+use DateTimeImmutable;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class FoldingCommand extends Command
 {
     protected static $defaultName = 'app:get:team:stats';
     private FoldingTeamsApiManager $fcm;
+    private int $foldingTeamNumber;
 
     /**
      * Constructor
      *
      * @param FoldingTeamsApiManager $fcm
+     * @param int $foldingTeamNumber
      */
-    public function __construct(FoldingTeamsApiManager $fcm)
+    public function __construct(FoldingTeamsApiManager $fcm, int $foldingTeamNumber)
     {
         $this->fcm = $fcm;
+        $this->foldingTeamNumber = $foldingTeamNumber;
         parent::__construct();
     }
 
@@ -47,16 +52,45 @@ class FoldingCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeLn('Total current Folding@Home teams amount: '.$this->fcm->getCurrentTotalTeams());
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Welcome to Folding@Home '.$this->getName().' command line tool');
+        $io->section('Total current Folding@Home teams amount');
+        $io->text($this->fcm->getCurrentTotalTeams());
         $team = $this->fcm->getFoldingTeamById($input->getArgument('id'));
-        $output->writeLn($team);
+        $io->section('Team report');
+        $io->table(
+            ['#', 'Name', 'Members', 'Score', 'Work Units', 'Rank'],
+            [
+                [
+                    $team->getId(),
+                    $team->getName(),
+                    count($team->getMemberAccounts()),
+                    $team->getScore(),
+                    $team->getWus(),
+                    $team->getRank(),
+                ],
+            ]
+        );
         if (count($team->getMemberAccounts()) > 0) {
-            $output->writeln('Team member accounts:');
+            $rows = [];
+            $io->section('Team member accounts');
             /** @var FoldingTeamMemberAccount $teamMemberAccount */
             foreach ($team->getMemberAccounts() as $teamMemberAccount) {
-                $output->writeLn($teamMemberAccount);
+                $rows[] = [
+                    $teamMemberAccount->getId(),
+                    $teamMemberAccount->getName(),
+                    $teamMemberAccount->getScore(),
+                    $teamMemberAccount->getWus(),
+                    $teamMemberAccount->getRank(),
+                ];
             }
+            $io->table(
+                ['#', 'Name', 'Score', 'Work Units', 'Rank'],
+                $rows
+            );
         }
+        $now = new DateTimeImmutable();
+        $io->success('Reported data status at '.$now->format('d/m/Y H:i'));
 
         return 1;
     }
